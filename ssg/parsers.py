576863027fbeeee9fc2af05604e83978330e1f9c
Patrick Davies <patrick.davies@daugherty.com>
2022-03-11 15:22:59 -0600

    module 4 complete

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -53,6 +53,8 @@ class MarkdownParser(Parser):
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
 
+        hooks.event("written")
+
 
 class ReStructuredTextParser(Parser):
     file_exts = [".rst"]
@@ -65,3 +67,5 @@ class ReStructuredTextParser(Parser):
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
+
+        hooks.event("written")
